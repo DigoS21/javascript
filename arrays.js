e15741f9f75098396418075e9eba6fb8f50f951b
DigoS21 <rodrigo.pereira.it2b@gmail.com>
2024-08-13 19:21:59 +0000

    alterações da aula

--- arrays.js
+++ arrays.js
@@ -1,20 +1,6 @@
 // Arrays - estudo sobre a utilização 
 
-// const alunos = [
-//     {
-//         nome: "Rodrigo",
-//         idade: 33,
-//     },
-
-//     {
-//         nome: "Carem",
-//         idade: 32,
-//     },
-// ];
-
-// console.log(alunos[1].idade);
-
-const frutas = ["Maça", "Banana", "Perâ", "Manga"];
+//const frutas = ["Maça", "Banana", "Perâ", "Manga"];
 
 //console.log(frutas);
 
@@ -22,10 +8,47 @@ const frutas = ["Maça", "Banana", "Perâ", "Manga"];
 //     console.log(frutas[]);
 // });
 
-for(let i = 0; i < 4; i++){
-    console.log(frutas[i]);
-};
+// for(let i = 0; i < 4; i++){
+//     console.log(frutas[i]);
+// };
 
 //console.log(frutas[1]);
 //console.log(typeof(frutas));
 
+const alunos = [
+    {
+        nome: "Rodrigo",
+        idade: 33,
+    },
+
+    {
+        nome: "Carem",
+        idade: 32,
+    },
+
+    {
+        nome: "Carine",
+        idade: 1,
+    },
+
+    {
+        nome: "Juliana",
+        idade: 28,
+    },
+
+    {
+        nome: "José",
+        idade: 45,
+    },
+
+    {
+        nome: "Roberto",
+        idade: 16,
+    },
+];
+
+//const alunosfiltrados = alunos.filter(aluno => aluno.idade > 18);
+//console.table(alunosfiltrados);
+
+const alunosPesquisados = alunos.find(aluno => aluno.nome === 'Roberto');
+console.table(alunosPesquisados);
