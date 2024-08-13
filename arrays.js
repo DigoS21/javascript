551a916e168071e9243953e7a49dba1f57856147
DigoS21 <rodrigo.pereira.it2b@gmail.com>
2024-08-13 18:11:00 +0000

    Novos arquivos javascript

---
+++ arrays.js
@@ -0,0 +1,31 @@
+// Arrays - estudo sobre a utilização 
+
+// const alunos = [
+//     {
+//         nome: "Rodrigo",
+//         idade: 33,
+//     },
+
+//     {
+//         nome: "Carem",
+//         idade: 32,
+//     },
+// ];
+
+// console.log(alunos[1].idade);
+
+const frutas = ["Maça", "Banana", "Perâ", "Manga"];
+
+//console.log(frutas);
+
+// frutas.forEach((value) =>{
+//     console.log(frutas[]);
+// });
+
+for(let i = 0; i < 4; i++){
+    console.log(frutas[i]);
+};
+
+//console.log(frutas[1]);
+//console.log(typeof(frutas));
+
--- script.js
+++ script.js
@@ -1,1 +1,30 @@
+/* Função padrão  */ 
+
+
+// function subtrair(num1,num2) {
+//     return num1 - num2; 
+// }
+
+// function somar(num1,num2) {
+//     return num1 + num2; 
+// }
+
+/* Arrow Function com {} */ 
+
+// const subtrair = (num1,num2) => {
+//     return num1 - num2; 
+// }
+
+// const somar = (num1,num2) => {
+//     return num1 + num2; 
+// }
+
+/* Arrow Function sem aspas {} */ 
+
+const subtrair = (num1,num2) => num1 - num2; 
+
+const somar = (num1,num2) => num1 + num2; 
+
+const resultado = 'Resultado Final Soma: ' + somar(20,3) + ' Subtração: '+ subtrair(15,6);
+console.log(resultado);
 
